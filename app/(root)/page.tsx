f9b51406a1d684fd4c3ad6957b3f1c6c3c524159
rbpolim <rbpolim@gmail.com>
2023-07-15 16:09:25 -0300

    chore: created sign-in & sign-up folders (clerk)

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-6">
+      <Button variant="link">
+        Hello
+      </Button>
+    </div>
+  )
+}
