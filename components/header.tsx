ba3e9fe007baa10ca9480feb4eea573905b59bdc
rbpolim <rbpolim@gmail.com>
2023-07-28 12:54:17 -0300

    feat: added main-nav into header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,15 @@
 import { UserButton } from "@clerk/nextjs";
 
+import { MainNav } from "@/components/main-nav";
+
 export function Header() {
   return (
     <div className="border-b shadow-sm">
-      <div className="flex h-20 items-center px-4">
-        <h1 className="font-black text-4xl">Supply Marine</h1>
+      <div className="flex items-center h-16 px-4">
+        <h2 className="font-black text-2xl">
+          Supply Marine
+        </h2>
+        <MainNav className="mx-10" />
 
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
