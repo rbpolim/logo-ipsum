2ce85a8562903032b3156c11a1f083e455480010
rbpolim <rbpolim@gmail.com>
2023-07-30 19:41:09 -0300

    feat: created columns component (orders)

---
+++ app/(dashboard)/orders/components/columns.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+import { CellAction } from "./cell-action"
+import { Badge } from "@/components/ui/badge"
+
+export type OrderColumn = {
+  id: string
+  number: number
+  company: string
+  unit: string
+  status: string
+  dateStart: string
+}
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "number",
+    header: "Order",
+  },
+  {
+    accessorKey: "company",
+    header: "Company",
+  },
+  {
+    accessorKey: "unit",
+    header: "Unit",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => <Badge>{row.original.status}</Badge>
+  },
+  {
+    accessorKey: "dateStart",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />
+  },
+]
