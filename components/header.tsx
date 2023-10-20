cc68f96923b860d311fb0ce1d908d72235b5b4b2
rbpolim <rbpolim@gmail.com>
2023-10-20 09:31:08 -0300

    chore: updated header w/ logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,15 @@
 import { UserButton } from "@clerk/nextjs";
 
+import { Logo } from "@/components/logo";
 import { NavRoutes } from "@/components/nav-routes";
-import { ModeToggle } from "./mode-toggle";
-import { MobileSidebar } from "./mobile-sidebar";
+import { ModeToggle } from "@/components/mode-toggle";
+import { MobileSidebar } from "@/components/mobile-sidebar";
 
 export function Header() {
   return (
     <header className="w-full border-b shadow-sm">
       <div className="flex items-center h-16 px-4">
-        <h2 className="hidden text-2xl font-black md:flex">
-          Supply Marine
-        </h2>
+        <Logo />
         <MobileSidebar />
         <NavRoutes className="mx-10" />
         <div className="flex items-center ml-auto space-x-4">
