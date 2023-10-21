7137ef44c2cdda1dbed4d7ed795b496502e54a14
rbpolim <rbpolim@gmail.com>
2023-10-21 08:03:12 -0300

    feat: render placeholder data table

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -53,7 +53,7 @@ export function DataTable<TData, TValue>({
     <div>
       <div className="flex items-center py-4">
         <Input
-          placeholder="Search"
+          placeholder={`Search by ${searchKey}`}
           value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
             table.getColumn(searchKey)?.setFilterValue(event.target.value)
