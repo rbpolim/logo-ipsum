8837ebc577703aa24ac10eba0f1649240f763485
rbpolim <rbpolim@gmail.com>
2023-10-26 14:18:16 -0300

    feat: fn addYearPrefixToId

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function addYearPrefixToId(id: number) {
+  const currentYear = new Date().getFullYear().toString()
+  const paddedId = id.toString().padStart(4, '0')
+  
+  return `${currentYear}${paddedId}`
+}
