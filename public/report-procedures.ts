9b1b6e89bf877d00e2175e5cdb46b8938999106e
rbpolim <rbpolim@gmail.com>
2023-11-03 11:10:54 -0300

    feat: options for procedures (static-data)

---
+++ public/report-procedures.ts
@@ -0,0 +1,51 @@
+export const optionsProcedures = [
+  {
+    code: 'PR-EG-01',
+    description: '01. Limpeza interna do sistema frigorígeno',
+  },
+  {
+    code: 'PR-EG-02',
+    description: '02. Regulagem de sub-resfriamento e superaquecimento',
+  },
+  { code: 'PR-EG-03', description: '03. Inserção de fluido refrigerante' },
+  { code: 'PR-EG-04', description: '04. Despressurização do equipamento' },
+  { code: 'PR-EG-05', description: '05. Inspeção de vazamento' },
+  { code: 'PR-EG-06', description: '06. Reparo de ponto de vazamento' },
+  { code: 'PR-EG-07', description: '07. Medição de corrente' },
+  {
+    code: 'PR-EG-08',
+    description: '08. Medição de pressão de alta e baixa',
+  },
+  { code: 'PR-EG-09', description: '09. Medição de pressão de óleo' },
+  { code: 'PR-EG-10', description: '10. Medição de tensão' },
+  { code: 'PR-EG-11', description: '11. Pressurização com nitrogênio' },
+  {
+    code: 'PR-EG-12',
+
+    description: '12. Recolhimento de fluido refrigerante: Sistema x Cilindro',
+  },
+  {
+    code: 'PR-EG-13',
+
+    description: '13. Recolhimento de fluido refrigerante: Sistema x Sistema',
+  },
+  { code: 'PR-EG-14', description: '14. Remoção de óleo lubrificante' },
+  { code: 'PR-EG-15', description: '15. Procedimento de Vácuo' },
+  { code: 'PR-EG-16', description: '16. Inspeção de compressor' },
+  { code: 'PR-EG-17', description: '17. Montagem de compressor' },
+  { code: 'PR-EG-18', description: '18. Desumidificação' },
+  { code: 'PR-EG-19', description: '19. Inserção de óleo lubrificante' },
+  { code: 'PR-EG-20', description: '20. Limpeza de Dutos' },
+  { code: 'PR-EG-21', description: '21. Limpeza de Coifas' },
+  { code: 'PR-EG-22', description: '22. Coleta para análise de Ar' },
+  {
+    code: 'PR-EG-23',
+    description: '23. Teste de estanqueidade do sistema',
+  },
+  {
+    code: 'PR-EG-24',
+    description: '24. Teste de estanqueidade do compressor',
+  },
+  { code: 'PR-EG-25', description: '25. Procedimento de Solda Brasagem' },
+  { code: 'PR-EG-26', description: '26. Procedimento de Pintura' },
+]
